a7ca8e8c3d42d326cd25d90f09145cc06c0a8361
Abhiaws26 <abhiaws2609@gmail.com>
2025-04-21 12:03:00 +0000

    flaskapp

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
 echo "console.log('Hello from EC2');" > app.js
+=======
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Jenkins + Docker on EC2!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+>>>>>>> 2733635 (flaskapp)
